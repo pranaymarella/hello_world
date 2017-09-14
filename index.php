486ee38d7236c30eab92f449fc86b285fe7423c7
Pranay Marella <pranaylearn@gmail.com>
2017-09-13 21:18:36 -0500

    testing

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1>Hello World</h1>
 
-<img src='https://i.ytimg.com/vi/J5OSRpRyl6g/maxresdefault.jpg' alt="image"/>
+<img src='https://i.ytimg.com/vi/J5OSRpRyl6g/maxresdefault.jpg' width="100px" alt="image"/>
